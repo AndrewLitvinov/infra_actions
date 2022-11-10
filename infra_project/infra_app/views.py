53dbd9e29dbdbe5b32b85b34d088934f3de888b4
Андрей Литвинов <and1234567890@yandex.ru>
2022-11-10 16:50:09 +0300

    6

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -1,13 +1,13 @@
 from http import HTTPStatus
 
-# from django.http import HttpResponse
+from django.http import HttpResponse
 
 
 def index(request):
-    # return HttpResponse('У меня получилось!')
-    HTTPStatus.OK
+    return HttpResponse(status=HTTPStatus.OK)
 
 
 def second_page(request):
     # return HttpResponse('А это вторая страница')
-    HTTPStatus.OK
+    # HTTPStatus.OK
+    return HttpResponse(status=HTTPStatus.OK)
