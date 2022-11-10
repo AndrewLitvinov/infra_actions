c91b43224d01ce157b9304681a383688486099e3
Андрей Литвинов <and1234567890@yandex.ru>
2022-11-10 20:40:39 +0300

    13

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('У меня получилось2!')
+    return HttpResponse('У меня получилось3!')
 
 
 def second_page(request):
